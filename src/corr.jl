835d6e307654a36e0ddc5d3c060f8f58ca8a87b0
EdwardBerman <10emb29@gmail.com>
2024-07-18 10:26:26 -0400

    progress

--- src/corr.jl
+++ src/corr.jl
@@ -2,16 +2,29 @@ module astro
     export corr
     using LinearAlgebra
     using Statistics
+    using Clustering
     using Base.Iterators: product
     using Base.Iterators: partition
 
     include("metrics.jl")
     # Vector of vectors, vector of floats,
     
+
+    #Heirarchical Clustering from Julia
+    
     function treecorr()
         #initialize partition sizes and positions of 2 cells
         return 0
     end
+    
+
+    function clustercorr(;metric=euclidean)
+        return 0
+    end
+
+    #=
+    Correlators: TreeCorr, Heirarchical Clustering, etc.
+    =#
 
     function corr(x::Vector{Vector{Float64}}, y::Vector{Float64} ;spacing=log, metric=euclidean, correlator=treecorr)
         return correlator(x, y)
