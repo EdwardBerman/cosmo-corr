573ae0e83ced150dcbe8e6e8a2d3b83058727442
EdwardBerman <10emb29@gmail.com>
2024-07-18 15:53:36 -0400

    naive corr

--- src/corr.jl
+++ src/corr.jl
@@ -1,6 +1,7 @@
-module astro
+module astrocorr
     export corr
     using LinearAlgebra
+    using Base.Threads
     using Statistics
     using Clustering
     using Distances
@@ -28,7 +29,7 @@ module astro
     
     
     #Advantage of cluster corr is that you can specify the exact number of bins you want, where as in treecorr, you can't. Bin at granularity that loses info. The disadvantage is that you don't have a bound on the binning error.
-    function clustercorr(x, y; spacing=log, metric=Euclidean())
+    function clustercorr(ra, dec, corr1, corr2; spacing=log, metric=Euclidean())
         distance_matrix = build_distance_matrix(x, y, metric=metric)
         distance_matrix = spacing.(distance_matrix)
         distance_vector = reshape(distance_matrix, :, 1)
@@ -45,27 +46,75 @@ module astro
         end
     end
 
-    function naivecorr(x, y; metric=euclidean)
-        return mean([metric(x[i], y[j]) for i in 1:length(x), j in 1:length(y)])
+    function naivecorr(ra, dec, corr1, corr2, θ_min, number_bins, θ_max; spacing=log, metric=Euclidean())
+        distance_matrix = build_distance_matrix(ra, dec, metric=metric)
+        distance_matrix = spacing.(distance_matrix)
+        distance_vector = reshape(distance_matrix, :, 1)
+        
+        θ_bins = range(θ_min, stop=θ_max, length=number_bins)
+
+        #drop elements of the distance_vector into different θ bins
+        θ_bin_assignments = zeros(length(distance_vector))
+        for i in 1:length(distance_vector)
+            for j in 1:length(θ_bins)
+                if distance_vector[i] < θ_bins[j]
+                    θ_bin_assignments[i] = j
+                    break
+                end
+            end
+        end
+
+        θ_bin_means = zeros(number_bins)
+        for i in 1:number_bins
+            bin = findall(θ_bin_assignments .== i)
+            mean_theta = mean(distance_vector[bin])
+        end
+        
+        df = DataFrame(bin_number=Int[], min_distance=Float64[], max_distance=Float64[], count=Int[], mean_distance=Float64[], corr1=Vector{Float64}[], corr2=Vector{Float64}[])
+        
+        for i in 1:number_bins
+            bin = findall(θ_bin_assignments .== i)
+            min_distance = minimum(distance_vector[bin])
+            max_distance = maximum(distance_vector[bin])
+            count = length(bin)
+            mean_distance = mean(distance_vector[bin])
+            corr1_values = corr1[bin]
+            corr2_values = corr2[bin]
+
+            push!(df, (bin_number=i, min_distance=min_distance, max_distance=max_distance, count=count, mean_distance=mean_distance, corr1=corr1_values, corr2=corr2_values))
+        end
+
+        if verbose
+            println(df)
+        end
+        
+        ψ_θ = zeros(2, number_bins) 
+        @threads for i in 1:number_bins
+            ψ_θ[1,i] = df[i, :mean_distance]
+            ψ_θ[2,i] = mean(metric(df[i, :corr1], df[i, :corr2]))
+        end
+
+        return ψ_θ
     end
+    # parallelize ξ (θ) for different θ bins
 
     #=
     Correlators: TreeCorr, Heirarchical Clustering, etc.
     =#
 
-    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Vector{Float64}}, y::Vector{Float64}, number_bins::Int64; spacing=log, metric=Euclidean(), correlator=treecorr)
+    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Vector{Float64}}, y::Vector{Float64}, θ_min::Float64, number_bins::Int64, θ_max::Float64; spacing=log, metric=Euclidean(), correlator=treecorr)
         return correlator(x, y)
     end
     
-    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Float64}, y::Vector{Vector{Float64}}, number_bins::Int64 ;spacing=log, metric=Euclidean(), correlator=treecorr)
+    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Float64}, y::Vector{Vector{Float64}}, θ_min::Float64, number_bins::Int64, θ_max::Float64; spacing=log, metric=Euclidean(), correlator=treecorr)
         return correlator(x, y)
     end
 
-    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Vector{Complex{Float64}}}, y::Vector{Float64}, number_bins::Int64 ;spacing=log, metric=Euclidean(), correlator=treecorr)
+    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Vector{Complex{Float64}}}, y::Vector{Float64}, θ_min::Float64, number_bins::Int64, θ_max::Float64; spacing=log, metric=Euclidean(), correlator=treecorr)
         return correlator(x, y)
     end
     
-    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Float64}, y::Vector{Vector{Float64}}, number_bins::Int64 ;spacing=log, metric=Euclidean(), correlator=treecorr)
+    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Float64}, y::Vector{Vector{Float64}}, θ_min::Float64, number_bins::Int64, θ_max::Float64; spacing=log, metric=Euclidean(), correlator=treecorr)
         return correlator(x, y)
     end
     #=
