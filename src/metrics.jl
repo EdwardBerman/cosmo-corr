286ac1333171d383dde1426d95ff490f5293f616
EdwardBerman <10emb29@gmail.com>
2024-07-18 18:25:36 -0400

    better way to filter, less calculations

--- src/metrics.jl
+++ src/metrics.jl
@@ -5,9 +5,7 @@ function build_distance_matrix(ra, dec, metric=Euclidean())
     distance_matrix = zeros(length(ra), length(dec))
     @threads for i in 1:length(ra)
         for j in 1:length(dec)
-            if j > i
-                distance_matrix[i, j] = NaN
-            else
+            if j ≤ i
                 distance_matrix[i,j] = metric(ra[i], dec[j])
             end
         end
