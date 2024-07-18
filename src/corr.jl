09cec3848f31d1a97f32f9604b2fcdf9eb2710a1
EdwardBerman <10emb29@gmail.com>
2024-07-18 13:47:32 -0400

    cooking

--- src/corr.jl
+++ src/corr.jl
@@ -3,6 +3,8 @@ module astro
     using LinearAlgebra
     using Statistics
     using Clustering
+    using Distances
+    using DataFrames
     using Base.Iterators: product
     using Base.Iterators: partition
 
@@ -26,10 +28,21 @@ module astro
     
     
     #Advantage of cluster corr is that you can specify the exact number of bins you want, where as in treecorr, you can't. Bin at granularity that loses info. The disadvantage is that you don't have a bound on the binning error.
-    function clustercorr(x, y; spacing=log, metric=euclidean)
+    function clustercorr(x, y; spacing=log, metric=Euclidean())
         distance_matrix = build_distance_matrix(x, y, metric=metric)
-        # log scale all entries
+        distance_matrix = spacing.(distance_matrix)
+        distance_vector = reshape(distance_matrix, :, 1)
         hclusters = hclust(distance_matrix)
+        θ_bin_assigments = cutree(hclusters, k=number_bins)
+        #df = |min theta in bin _i | max theta in bin _i | index of bin _i | number of points in bin _i
+        df = DataFrame()
+        for i in 1:number_bins
+            bin = findall(θ_bin_assigments .== i)
+            push!(df, [minimum(distance_vector[bin]), maximum(distance_vector[bin]), i, length(bin)])
+        end
+        if verbose
+            println(df)
+        end
     end
 
     function naivecorr(x, y; metric=euclidean)
@@ -40,19 +53,19 @@ module astro
     Correlators: TreeCorr, Heirarchical Clustering, etc.
     =#
 
-    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Vector{Float64}}, y::Vector{Float64} ;spacing=log, metric=euclidean, correlator=treecorr)
+    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Vector{Float64}}, y::Vector{Float64}, number_bins::Int64; spacing=log, metric=Euclidean(), correlator=treecorr)
         return correlator(x, y)
     end
     
-    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Float64}, y::Vector{Vector{Float64}} ;spacing=log, metric=euclidean, correlator=treecorr)
+    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Float64}, y::Vector{Vector{Float64}}, number_bins::Int64 ;spacing=log, metric=Euclidean(), correlator=treecorr)
         return correlator(x, y)
     end
 
-    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Vector{Complex{Float64}}}, y::Vector{Float64} ;spacing=log, metric=euclidean, correlator=treecorr)
+    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Vector{Complex{Float64}}}, y::Vector{Float64}, number_bins::Int64 ;spacing=log, metric=Euclidean(), correlator=treecorr)
         return correlator(x, y)
     end
     
-    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Float64}, y::Vector{Vector{Float64}} ;spacing=log, metric=euclidean, correlator=treecorr)
+    function corr(ra::Vector{Float64}, dec::Vector{Float64}, x::Vector{Float64}, y::Vector{Vector{Float64}}, number_bins::Int64 ;spacing=log, metric=Euclidean(), correlator=treecorr)
         return correlator(x, y)
     end
     #=
