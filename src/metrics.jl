445daae9f49d9bf1b40c8f95524c46497a34f1f6
EdwardBerman <10emb29@gmail.com>
2024-07-18 21:44:14 -0400

    edge cases and debugging

--- src/metrics.jl
+++ src/metrics.jl
@@ -1,11 +1,11 @@
 #euclidean
 
 
-function build_distance_matrix(ra, dec, metric=Euclidean())
+function build_distance_matrix(ra, dec; metric=Euclidean())
     distance_matrix = zeros(length(ra), length(dec))
     @threads for i in 1:length(ra)
         for j in 1:length(dec)
-            if j ≤ i
+            if j < i
                 distance_matrix[i,j] = metric(ra[i], dec[j])
             end
         end
