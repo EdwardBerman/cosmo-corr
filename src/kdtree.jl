7b674957b26fdd5f9917a2cb32f191964ff05acd
EdwardBerman <10emb29@gmail.com>
2024-07-22 09:33:04 -0400

    boiler plate

--- src/kdtree.jl
+++ src/kdtree.jl
@@ -17,3 +17,17 @@ function append_right!(tree::kdtree{T}, node::Node{T}) where T
         append_right!(tree.root, node)
     end
 end
+
+function initialize_circles()
+    return centers, radii
+end
+
+function populate!(tree::kdtree{T}, galaxies::Vector{T}) where T
+    centers, radii = initialize_circles()
+    while condition
+        @threads for i in 1:size(galaxies, 1)
+            node = Node{T}(galaxies[i, :])
+            append_left!(tree, node)
+        end
+    end
+end
