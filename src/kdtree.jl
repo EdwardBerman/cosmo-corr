ffafc3fcd332f699e034225e8856368418bdb050
EdwardBerman <10emb29@gmail.com>
2024-07-22 09:39:59 -0400

    boiler plate

--- src/kdtree.jl
+++ src/kdtree.jl
@@ -1,5 +1,11 @@
-struct kdtree{T}
+mutable struct kdtree{T}
     root::Node{T}
+end
+
+mutable struct galaxy_circle{T, r, g}
+    center::Vector{T}
+    radius::r
+    galaxies::Vector{g}
 end
 
 function append_left!(tree::kdtree{T}, node::Node{T}) where T
@@ -22,9 +28,13 @@ function initialize_circles()
     return centers, radii
 end
 
+function split_cirlces(galaxy_circles::Vector{galaxy_circle})
+    return left_circles, right_circles
+end
+
 function populate!(tree::kdtree{T}, galaxies::Vector{T}) where T
     centers, radii = initialize_circles()
-    while condition
+    while condition # no circles need to be split 
         @threads for i in 1:size(galaxies, 1)
             node = Node{T}(galaxies[i, :])
             append_left!(tree, node)
