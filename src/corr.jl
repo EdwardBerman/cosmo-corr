0f93a43af2ef15603bcda85d0fdc158c547c4b86
EdwardBerman <10emb29@gmail.com>
2024-07-31 09:31:03 -0400

    naivecorr and treecorr just need to be tested now

--- src/corr.jl
+++ src/corr.jl
@@ -28,18 +28,98 @@ module astrocorr
     #Heirarchical Clustering from Julia
     
     function treecorr(ra, dec, corr1, corr2, θ_min, number_bins, θ_max; spacing=log, sky_metric=Vincenty_Formula, verbose=true)
+        @assert length(ra) == length(dec) == length(corr1) == length(corr2) "ra, dec, corr1, and corr2 must be the same length"
         sky_metric = Vincenty_Formula()
         galaxies = [Galaxy(ra[i], dec[i], corr1[i], corr2[i]) for i in 1:length(ra)]
         tree = populate(galaxies, metric=sky_metric)
         leafs = get_leaves(tree)
-        #initialize partition sizes and positions of 2 cells
-        # Naively separate into line above some y axis and below some y axis, if the split is lopsided, rotate the axis
+        ra_circles = [leaf.root.center[1] for leaf in leafs]
+        dec_circles = [leaf.root.center[2] for leaf in leafs]
+        distance_matrix = build_distance_matrix(ra_circles, dec_circles, metric=sky_metric)
+        distance_matrix = spacing.(distance_matrix)
+
+        n = length(ra_circles)
+        indices = [(i, j) for i in 1:n, j in 1:n if j < i]
+        distance_vector = [distance_matrix[i, j] for (i, j) in indices]
+        
+        θ_bins = range(θ_min, stop=θ_max, length=number_bins)
+
+        θ_bin_assignments = zeros(length(distance_vector))
+        for i in 1:length(distance_vector)
+            for j in 1:length(θ_bins)
+                if distance_vector[i] < θ_bins[j]
+                    θ_bin_assignments[i] = j
+                    break
+                end
+            end
+        end
+
+        df = DataFrame(bin_number=Int[], 
+                       min_distance=Float64[], 
+                       max_distance=Float64[], 
+                       count=Int[], 
+                       mean_distance=Float64[], 
+                       corr1=Vector{Float64}[], 
+                       corr2=Vector{Float64}[], 
+                       corr1_reverse=Vector{Float64}[], 
+                       corr2_reverse=Vector{Float64}[])
+
+        for i in 1:number_bins
+            bin = findall(θ_bin_assignments .== i)
+            if !isempty(bin)
+                min_distance = minimum(distance_vector[bin])
+                max_distance = maximum(distance_vector[bin])
+                count = length(bin)
+                mean_distance = mean(distance_vector[bin])
+                bin_indices = [indices[k] for k in bin]
+
+                corr1_values = []
+                corr2_values = []
+
+                corr1_reverse_values = []
+                corr2_reverse_values = []
+
+                for (i, j) in bin_indices
+                    append!(corr1_values, [galaxies[k].corr1 for k in leafs[i].root.galaxies])
+                    append!(corr2_values, [galaxies[k].corr2 for k in leafs[j].root.galaxies])
+                    append!(corr1_reverse_values, [galaxies[k].corr1 for k in leafs[j].root.galaxies])
+                    append!(corr2_reverse_values, [galaxies[k].corr2 for k in leafs[i].root.galaxies])
+                end
+
+                push!(df, (bin_number=i, 
+                           min_distance=min_distance, 
+                           max_distance=max_distance, 
+                           count=count, 
+                           mean_distance=mean_distance, 
+                           corr1=corr1_values, 
+                           corr2=corr2_values, 
+                           corr1_reverse=corr1_reverse_values, 
+                           corr2_reverse=corr2_reverse_values))
+            end
+        end
+        
+        if verbose
+            println(df)
+        end
+        
+        ψ_θ = zeros(2, number_bins) 
+        @threads for i in 1:nrow(df)
+            ψ_θ[1,i] = df[i, :mean_distance]
+            c1 = df[i, :corr1]
+            c2 = df[i, :corr2]
+            c3 = df[i, :corr1_reverse]
+            c4 = df[i, :corr2_reverse]
+            ψ_θ[2,i] = sum(c1 * c2') + sum(c3 * c4') / (length(c1 * c2') + length(c3 * c4')) # mean of the two outer products
+        end
+        return ψ_θ
+
         return 0
     end
     
     
     #Advantage of cluster corr is that you can specify the exact number of bins you want, where as in treecorr, you can't. Bin at granularity that loses info. The disadvantage is that you don't have a bound on the binning error.
     function clustercorr(ra, dec, corr1, corr2, θ_min, number_bins, θ_max; spacing=log, sky_metric=Vincenty_Formula, verbose=true)
+        @assert length(ra) == length(dec) == length(corr1) == length(corr2) "ra, dec, corr1, and corr2 must be the same length"
         distance_matrix = build_distance_matrix(x, y, metric=metric)
         distance_matrix = spacing.(distance_matrix)
         distance_vector = reshape(distance_matrix, :, 1) # probably don't do this actually. 2 x ? matrix
@@ -59,7 +139,6 @@ module astrocorr
     function naivecorr(ra, dec, corr1, corr2, θ_min, number_bins, θ_max; spacing=log, sky_metric=Vincenty_Formula, verbose=true)
         @assert length(ra) == length(dec) == length(corr1) == length(corr2) "ra, dec, corr1, and corr2 must be the same length"
         distance_matrix = build_distance_matrix(ra, dec, metric=sky_metric)
-        distance_matrix *= 3600
         distance_matrix = spacing.(distance_matrix)
 
         n = length(ra)
@@ -78,7 +157,15 @@ module astrocorr
             end
         end
 
-        df = DataFrame(bin_number=Int[], min_distance=Float64[], max_distance=Float64[], count=Int[], mean_distance=Float64[], corr1=Vector{Float64}[], corr2=Vector{Float64}[])
+        df = DataFrame(bin_number=Int[], 
+                       min_distance=Float64[], 
+                       max_distance=Float64[], 
+                       count=Int[], 
+                       mean_distance=Float64[], 
+                       corr1=Vector{Float64}[], 
+                       corr2=Vector{Float64}[], 
+                       corr1_reverse=Vector{Float64}[], 
+                       corr2_reverse=Vector{Float64}[])
         
         for i in 1:number_bins
             bin = findall(θ_bin_assignments .== i)
@@ -90,8 +177,18 @@ module astrocorr
                 bin_indices = [indices[k] for k in bin]
                 corr1_values = [corr1[i] for (i, j) in bin_indices]
                 corr2_values = [corr2[j] for (i, j) in bin_indices]
+                corr1_reverse_values = [corr1[j] for (i, j) in bin_indices]
+                corr2_reverse_values = [corr2[i] for (i, j) in bin_indices]
 
-                push!(df, (bin_number=i, min_distance=min_distance, max_distance=max_distance, count=count, mean_distance=mean_distance, corr1=corr1_values, corr2=corr2_values))
+                push!(df, (bin_number=i, 
+                           min_distance=min_distance, 
+                           max_distance=max_distance,
+                           count=count, 
+                           mean_distance=mean_distance, 
+                           corr1=corr1_values, 
+                           corr2=corr2_values,
+                           corr1_reverse=corr1_reverse_values,
+                           corr2_reverse=corr2_reverse_values))
             end
         end
 
@@ -102,9 +199,12 @@ module astrocorr
         ψ_θ = zeros(2, number_bins) 
         @threads for i in 1:nrow(df)
             ψ_θ[1,i] = df[i, :mean_distance]
-            ψ_θ[2,i] = mean(df[i, :corr1] * df[i, :corr2]')
+            c1 = df[i, :corr1]
+            c2 = df[i, :corr2]
+            c3 = df[i, :corr1_reverse]
+            c4 = df[i, :corr2_reverse]
+            ψ_θ[2,i] = sum(c1 * c2') + sum(c3 * c4') / (length(c1 * c2') + length(c3 * c4')) # mean of the two outer products
         end
-
         return ψ_θ
     end
     # parallelize ξ (θ) for different θ bins 
