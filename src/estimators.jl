a86125a94d0dcb872abaa4d40889853da5a6eb45
EdwardBerman <10emb29@gmail.com>
2024-08-01 17:13:02 -0400

    cleanup

--- src/estimators.jl
+++ src/estimators.jl
@@ -4,12 +4,7 @@ export landy_szalay_estimator
 
 using Distributions
 
-
-DD(c1,c2,c3,c4) = sum(c1 * c2') + sum(c3 * c4') / (length(c1 * c2') + length(c3 * c4'))
-function corr_metric_default_point_point(c1,c2,c3,c4)
-    DD = sum(c1 * c2') + sum(c3 * c4') / (length(c1 * c2') + length(c3 * c4'))
-    number_random_galaxies = length(c1)
-
+landy_szalay_estimator(DD, DR, RR) = (DD - 2*DR + RR) / RR
 
 
 end
