1b54131d67b258358741c833cffc08363eb62cd1
EdwardBerman <10emb29@gmail.com>
2024-08-03 12:05:49 -0400

    .

--- src/kmean.jl
+++ src/kmean.jl
@@ -46,15 +46,57 @@ function hcc(galaxies::Vector{Galaxy}, clusters::Int64, sky_metric=Vincenty_Form
         distance_matrix = build_distance_matrix([galaxy_circle.ra for galaxy_circle in galaxy_circles], [galaxy_circle.dec for galaxy_circle in galaxy_circles], sky_metric=sky_metric)
         distance_matrix = distance_matrix + distance_matrix'
         hc = hclust(distance_matrix)
-        clusters = cutree(hc, k=clusters)
+        cluster_labels = cutree(hc, k=clusters)
+
         galaxy_clusters = Dict{Int, Vector{Tuple{Int, Galaxy}}}()
+        for (i, galaxy_circle) in enumerate(galaxy_circles)
+            cluster = cluster_labels[i]
+            if !haskey(galaxy_clusters, cluster)
+                galaxy_clusters[cluster] = []
+            end
+            for galaxy in galaxy_circle.galaxies
+                append!(galaxy_clusters[cluster], galaxy)
+            end
+        end
+
+        galaxy_circles = []
+        for (cluster, galaxies_in_cluster) in galaxy_clusters
+            ra_list = [galaxy.ra for galaxy in galaxies_in_cluster]
+            dec_list = [galaxy.dec for galaxy in galaxies_in_cluster]
+            ra_circle = mean(ra_list)
+            dec_circle = mean(dec_list)
+            radius_circle = maximum([sky_metric([ra_circle, dec_circle], [galaxy.ra, galaxy.dec]) for galaxy in galaxies_in_cluster])
+            galaxy_circle = Galaxy_Circle([ra_circle, dec_circle],
+                                          radius_circle,
+                                          galaxies_in_cluster,
+                                          cluster,
+                                          false
+                                         )
+            push!(galaxy_circles, galaxy_circle)
+        end
+        
         for (i, cluster) in enumerate(clusters)
             if !haskey(galaxy_clusters, cluster)
                 galaxy_clusters[cluster] = []
             end
             push!(galaxy_clusters[cluster], (i, galaxies[i]))
         end
-    end
+        galaxy_circles = []
+        for (cluster, galaxy_info) in galaxy_clusters
+            ra_list = [galaxy.ra for (index, galaxy) in galaxy_info]
+            dec_list = [galaxy.dec for (index, galaxy) in galaxy_info]
+            ra_circle = mean(ra_list)
+            dec_circle = mean(dec_list)
+            radius_circle = maximum([sky_metric([ra_circle, dec_circle], [galaxy.ra, galaxy.dec]) for (index, galaxy) in galaxy_info])
+            galaxy_circle = Galaxy_Circle(
+                                          [ra_circle, dec_circle],
+                                          radius_circle,
+                                          [galaxy for (index, galaxy) in galaxy_info],
+                                          cluster,
+                                          false
+                                         )
+            push!(galaxy_circles, galaxy_circle)
+        end
 
     if verbose
         for (cluster, galaxy_info) in galaxy_clusters
