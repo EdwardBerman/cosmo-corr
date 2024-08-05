f393449dcedf4979f8b9bc872d76412db97c13d6
EdwardBerman <10emb29@gmail.com>
2024-08-05 09:59:14 -0400

    tuples are okay too

--- src/metrics.jl
+++ src/metrics.jl
@@ -25,6 +25,24 @@ function Vincenty_Formula(coord1::Vector{Float64}, coord2::Vector{Float64})
     return Δσ * (180 / π) 
 end
 
+function Vincenty_Formula(coord1::Tuple{Float64, Float64}, coord2::Tuple{Float64, Float64})
+    ϕ1, λ1 = coord1
+    ϕ2, λ2 = coord2
+    ϕ1 *= π / 180
+    λ1 *= π / 180
+    ϕ2 *= π / 180
+    λ2 *= π / 180
+    
+    Δλ = abs(λ2 - λ1)
+    c1 = (cos(ϕ2)*sin(Δλ))^2
+    c2 = (cos(ϕ1)*sin(ϕ2) - sin(ϕ1)*cos(ϕ2)*cos(Δλ))^2
+    c3 = sin(ϕ1)*sin(ϕ2) + cos(ϕ1)*cos(ϕ2)*cos(Δλ)
+    y = sqrt(c1 + c2)
+    x = c3
+    Δσ = atan(y, x)
+    return Δσ * (180 / π) 
+end
+
 struct Vincenty <: SemiMetric end
 
 function (d::Vincenty)(point1, point2)
