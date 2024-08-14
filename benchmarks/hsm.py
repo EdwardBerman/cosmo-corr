2db4817a6b0652727f1fe2d223096f4ee175d495
EdwardBerman <10emb29@gmail.com>
2024-08-14 09:41:12 -0400

    hsm

--- benchmarks/hsm.py
+++ benchmarks/hsm.py
@@ -14,54 +14,51 @@ class PSF_fit:
     sig_psfex: float
     g1_psfex: float
     g2_psfex: float
+    chi2: float
 
-# Loop over all visits
-for visit_number in range(1, 39):  # from 001 to 038
-    visit_str = f'visit_{visit_number:03d}'
-    fits_path = f'../../mccleary/real_images/gal_cutout_20mas_working/f150w/{visit_str}_nircam_f150w_COSMOS-Web_20mas_v0_6_i2d_valid_starcat.fits'
 
-    if not os.path.exists(fits_path):
-        print(f"File {fits_path} does not exist, skipping.")
+polydeg = 1
+polydim = 2
+nparams = np.prod([polydeg+i+1 for i in range(polydim)])/polydim
+
+psf_fits = []
+file_name = 'f444w_psf.fits'
+
+for i in range(len(f[2].data)):
+    try:
+        a_vignet = f[2].data['VIGNET'][i]
+        img_vignet = galsim.Image(a_vignet, wcs=galsim.PixelScale(0.03), xmin=0, ymin=0)
+        result_vignet = img_vignet.FindAdaptiveMom(guess_sig=1.0)
+        a_psfex = f[2].data['PSFEX_VIGNET'][i]
+        img_psfex = galsim.Image(a_psfex, wcs=galsim.PixelScale(0.03), xmin=0, ymin=0)
+        result_psfex = img_psfex.FindAdaptiveMom(guess_sig=1)
+        s_d, g1_d, g2_d = result_vignet.moments_sigma, result_vignet.observed_shape.g1, result_vignet.observed_shape.g2
+        s_p, g1_p, g2_p = result_psfex.moments_sigma, result_psfex.observed_shape.g1, result_psfex.observed_shape.g2
+        psf_fits.append(PSF_fit(s_d, g1_d, g2_d, s_p, g1_p, g2_p))
+        chi2_map = (a_vignet .- a_psfex ./ f[2].data['ERR_VIGNET'][i]).^2
+        chi2_finite = chi2_map[np.isfinite(chi2_map)]
+        ddof = chi2_finite.size - nparams
+        chi2 = np.sum(chi2_finite)/ddof
+        psf_fits.append(PSF_fit(s_d, g1_d, g2_d, s_p, g1_p, g2_p, chi2))
+    except Exception as e:
+        print(f"Error processing object {i} in {visit_str}: {e}")
         continue
 
-    f = fits.open(fits_path)
-    psf_fits = []
+dtype = [('sig_vignet', np.float64), ('g1_vignet', np.float64), ('g2_vignet', np.float64),
+         ('sig_psfex', np.float64), ('g1_psfex', np.float64), ('g2_psfex', np.float64)]
+psf_fits_array = np.array([tuple(psf.__dict__.values()) for psf in psf_fits], dtype=dtype)
+column_names = ['sig_vignet', 'g1_vignet', 'g2_vignet', 'sig_psfex', 'g1_psfex', 'g2_psfex']
+table = Table(psf_fits_array, names=column_names)
 
-    for i in range(len(f[2].data)):
-        try:
-            a_vignet = f[2].data['VIGNET'][i]
-            img_vignet = galsim.Image(a_vignet, wcs=galsim.PixelScale(0.02), xmin=0, ymin=0)
-            result_vignet = img_vignet.FindAdaptiveMom(guess_sig=1.0)
-            a_psfex = f[2].data['PSFEX_VIGNET'][i]
-            img_psfex = galsim.Image(a_psfex, wcs=galsim.PixelScale(0.02), xmin=0, ymin=0)
-            result_psfex = img_psfex.FindAdaptiveMom(guess_sig=1)
-            s_d, g1_d, g2_d = result_vignet.moments_sigma, result_vignet.observed_shape.g1, result_vignet.observed_shape.g2
-            s_p, g1_p, g2_p = result_psfex.moments_sigma, result_psfex.observed_shape.g1, result_psfex.observed_shape.g2
-            psf_fits.append(PSF_fit(s_d, g1_d, g2_d, s_p, g1_p, g2_p))
-        except Exception as e:
-            print(f"Error processing object {i} in {visit_str}: {e}")
-            continue
-
-    # Create a table of PSF fits for the current visit
-    dtype = [('sig_vignet', np.float64), ('g1_vignet', np.float64), ('g2_vignet', np.float64),
-             ('sig_psfex', np.float64), ('g1_psfex', np.float64), ('g2_psfex', np.float64)]
-    psf_fits_array = np.array([tuple(psf.__dict__.values()) for psf in psf_fits], dtype=dtype)
-    column_names = ['sig_vignet', 'g1_vignet', 'g2_vignet', 'sig_psfex', 'g1_psfex', 'g2_psfex']
-    table = Table(psf_fits_array, names=column_names)
-
-    # Data Cleaning - remove rows with NaN values
-    table = table[~np.isnan(table['sig_vignet'])]
-    table = table[~np.isnan(table['g1_vignet'])]
-    table = table[~np.isnan(table['g2_vignet'])]
-    table = table[~np.isnan(table['sig_psfex'])]
-    table = table[~np.isnan(table['g1_psfex'])]
-    table = table[~np.isnan(table['g2_psfex'])]
-
-    # Apply threshold for sig_vignet
-    threshold = 3*np.std(table['sig_vignet']) + np.mean(table['sig_vignet'])
-    table = table[table['sig_vignet'] < threshold]
-
-    # Save the table to a FITS file for the current visit
-    output_fits_path = f'psf_fits_{visit_str}.fits'
-    table.write(output_fits_path, overwrite=True)
-    print(f"Saved {output_fits_path}")
+table = table[~np.isnan(table['sig_vignet'])]
+table = table[~np.isnan(table['g1_vignet'])]
+table = table[~np.isnan(table['g2_vignet'])]
+table = table[~np.isnan(table['sig_psfex'])]
+table = table[~np.isnan(table['g1_psfex'])]
+table = table[~np.isnan(table['g2_psfex'])]
+threshold = 3*np.std(table['sig_vignet']) + np.mean(table['sig_vignet'])
+table = table[table['sig_vignet'] < threshold]
+table = table[table['chi2'] < 100]
+output_fits_path = f'psf_fits_{file_name}.fits'
+table.write(output_fits_path, overwrite=True)
+print(f"Saved {file_name}")
