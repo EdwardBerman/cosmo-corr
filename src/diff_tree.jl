59ae7118b69a2fb6187343065c8a51e21ccdc13b
EdwardBerman <10emb29@gmail.com>
2024-08-16 10:56:15 -0400

    Differentiable

--- src/diff_tree.jl
+++ src/diff_tree.jl
@@ -12,59 +12,67 @@ using Base.Threads
 using Zygote
 using Statistics
 
-function kd_tree(positions::Matrix{T}, quantities::Vector{S}, hyperparameters::Dict{Symbol, Any}) where {T, S}
+struct Galaxy 
+    ra::Float64
+    dec::Float64
+    corr1::Any
+    corr2::Any
+end
+
+function kd_tree(galaxies::Vector{T}, hyperparameters::Dict{Symbol, Any}) where T
     bin_size = hyperparameters[:bin_size]
     max_depth = hyperparameters[:max_depth]
     cell_minimum_count = hyperparameters[:cell_minimum_count]
+    spatial_dimensions = hyperparameters[:spatial_dimensions]
+    leaves = []
 
-    function build_tree(data, quantities, depth)
-        n, d = size(data)
+    function build_tree(galaxies, depth)
+        number_galaxies = length(galaxies)
         
-        if n <= bin_size || depth == max_depth || n <= cell_minimum_count
-            return positions, quantities
+        if number_galaxies <= bin_size || depth == max_depth || number_galaxies <= cell_minimum_count
+            push!(leaves, galaxies)
+            return galaxies
         end
     
-        ra_list = data[:, 1]
-        dec_list = data[:, 2]
-        if !isempty(ra_list) 
-            ra_extent = maximum(ra_list) - minimum(ra_list)
-        else
-            ra_extent = NaN
-        end
-        if !isempty(dec_list)
-            dec_extent = maximum(dec_list) - minimum(dec_list)
-        else
-            dec_extent = NaN
-        end
-        if ra_extent > dec_extent && ra_extent !== NaN && dec_extent !== NaN
+        ra_list = [galaxy.ra for galaxy in galaxies]
+        dec_list = [galaxy.dec for galaxy in galaxies]
+        ra_extent = maximum(ra_list) - minimum(ra_list)
+        dec_extent = maximum(dec_list) - minimum(dec_list)
+        
+        if ra_extent > dec_extent
             axis = 1
-        else ra_extent !== NaN && dec_extent !== NaN
+            median_value = median(ra_list)
+            galaxies_left = [galaxy for galaxy in galaxies if galaxy.ra <= median_value]
+            galaxies_right = [galaxy for galaxy in galaxies if galaxy.ra > median_value]
+        else 
             axis = 2
+            median_value = median(dec_list)
+            galaxies_left = [galaxy for galaxy in galaxies if galaxy.dec <= median_value]
+            galaxies_right = [galaxy for galaxy in galaxies if galaxy.dec > median_value]
         end
+            
 
-        median_value = median(data[:, axis])
-        data_left = data[data[:, axis] .<= median_value, :]
-        quantities_left = quantities[data[:, axis] .<= median_value]
-        data_right = data[data[:, axis] .> median_value, :]
-        quantities_right = quantities[data[:, axis] .> median_value]
-
-        left_tree = build_tree(data_left, quantities_left, depth + 1)
-        right_tree = build_tree(data_right, quantities_right, depth + 1)
-
-        return (left_tree, median_value, right_tree)
+        left_tree = build_tree(galaxies_left, depth + 1)
+        right_tree = build_tree(galaxies_right, depth + 1)
     end
 
-    tree = build_tree(data, quantities, 0)
+    tree = build_tree(galaxies, 0)
 
-    return tree
+    return leaves
 end
 
 positions = rand(100, 2)  # 100 points in 2D space
-quantities = rand(100)  # 100 random quantities
-hyperparameters = Dict{Symbol, Any}(:bin_size => 5, :max_depth => 10, :cell_minimum_count => 1)
-output = kd_tree(positions, quantities, hyperparameters)
+quantities_one = rand(100)  # 100 random quantities
+quantities_two = rand(100)  # 100 random quantities
+galaxies = [Galaxy(positions[i, 1], positions[i, 2], quantities_one[i], quantities_two[i]) for i in 1:size(positions, 1)]
+hyperparameters = Dict{Symbol, Any}(:bin_size => 5, :max_depth => 10, :cell_minimum_count => 1, :spatial_dimensions => 2)
+output = kd_tree(galaxies, hyperparameters)
+function estimator(leaves) 
+    c1 = [sum([galaxy.corr1 for galaxy in leaf]) for leaf in leaves]
+    c2 = [sum([galaxy.corr2 for galaxy in leaf]) for leaf in leaves]
+    return sum(c1 .* c2) / length(c1)
+end
+#grads = Zygote.gradient(kd_tree, galaxies, hyperparameters)
+grads = Zygote.gradient(estimator, output)
 
-
-tree, back = Zygote.pullback(kd_tree, data, hyperparameters)
-grads = back(ones(size(tree)))
-
+end
