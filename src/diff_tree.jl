7a29233d98a3604e7bb6abb9686ab0247e02cdde
EdwardBerman <10emb29@gmail.com>
2024-08-16 09:05:35 -0400

    diff

--- src/diff_tree.jl
+++ src/diff_tree.jl
@@ -11,18 +11,35 @@ using Base.Threads
 
 using Zygote
 
-function kd_tree(data::Matrix{T}, hyperparameters::Dict{Symbol, Any}) where T
+function kd_tree(positions::Matrix{T}, quantities::Vector{S}, hyperparameters::Dict{Symbol, Any}) where {T, S}
     bin_size = hyperparameters[:bin_size]
     max_depth = hyperparameters[:max_depth]
+    cell_minimum_count = hyperparameters[:cell_minimum_count]
 
-    function build_tree(data, depth)
+    function build_tree(data, quantities, depth)
         n, d = size(data)
         
-        if n <= bin_size || depth == max_depth
-            return data  # Return the data as a leaf
+        if n <= bin_size || depth == max_depth || n <= cell_minimum_count
+            return positions, quantities
         end
-
-        axis = argmax([maximum(data[:, i]) - minimum(data[:, i]) for i in 1:d])
+    
+        ra_list = data[:, 1]
+        dec_list = data[:, 2]
+        if !isempty(ra_list) 
+            ra_extent = maximum(ra_list) - minimum(ra_list)
+        else
+            ra_extent = NaN
+        end
+        if !isempty(dec_list)
+            dec_extent = maximum(dec_list) - minimum(dec_list)
+        else
+            dec_extent = NaN
+        end
+        if ra_extent > dec_extent && ra_extent !== NaN && dec_extent !== NaN
+            axis = 1
+        else ra_extent !== NaN && dec_extent !== NaN
+            axis = 2
+        end
 
         sorted_data = sort(data, by=row -> row[axis])
         median_idx = div(n, 2)
@@ -34,14 +51,15 @@ function kd_tree(data::Matrix{T}, hyperparameters::Dict{Symbol, Any}) where T
         return (left_tree, median_value, right_tree)
     end
 
-    tree = build_tree(data, 0)
+    tree = build_tree(data, quantities, 0)
 
     return tree
 end
 
-data = rand(100, 2)  # 100 points in 2D space
-hyperparameters = Dict{Symbol, Any}(:bin_size => 5, :max_depth => 10)
-output = kd_tree(data, hyperparameters)
+positions = rand(100, 2)  # 100 points in 2D space
+quantities = rand(100)  # 100 random quantities
+hyperparameters = Dict{Symbol, Any}(:bin_size => 5, :max_depth => 10, :cell_minimum_count => 1)
+output = kd_tree(positions, quantities, hyperparameters)
 
 
 tree, back = Zygote.pullback(kd_tree, data, hyperparameters)
