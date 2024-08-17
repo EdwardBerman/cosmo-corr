0aab284cc30a9f9b324a8076befdb076552043c7
EdwardBerman <10emb29@gmail.com>
2024-08-17 16:35:34 -0400

    break through, mean not median

--- src/diff_tree.jl
+++ src/diff_tree.jl
@@ -1,4 +1,5 @@
 using Zygote
+using Statistics
 
 struct Galaxy 
     ra::Float64
@@ -24,12 +25,19 @@ function build_tree(data::Vector{Galaxy}, hyperparams::hyperparameters; depth::I
         return TreeNode(nothing, nothing, data)
     end
     
-    # Determine the splitting point
-    mid = length(data) ÷ 2
-    
-    # Recursively build the left and right subtrees
-    left_subtree = build_tree(data[1:mid], hyperparams, depth=depth + 1)
-    right_subtree = build_tree(data[mid+1:end], hyperparams, depth=depth + 1)
+    ra_extent = maximum([galaxy.ra for galaxy in data]) - minimum([galaxy.ra for galaxy in data])
+    dec_extent = maximum([galaxy.dec for galaxy in data]) - minimum([galaxy.dec for galaxy in data])
+
+    if ra_extent > dec_extent
+        mean_ra = mean([galaxy.ra for galaxy in data])
+        left_subtree = build_tree([galaxy for galaxy in data if galaxy.ra <= mean_ra], hyperparams, depth=depth + 1)
+        right_subtree = build_tree([galaxy for galaxy in data if galaxy.ra > mean_ra], hyperparams, depth=depth + 1)
+    else
+        mean_dec = mean([galaxy.dec for galaxy in data])
+        left_subtree = build_tree([galaxy for galaxy in data if galaxy.dec <= mean_dec], hyperparams, depth=depth + 1)
+        right_subtree = build_tree([galaxy for galaxy in data if galaxy.dec > mean_dec], hyperparams, depth=depth + 1)
+    end
+
     
     return TreeNode(left_subtree, right_subtree, data)
 end
@@ -41,6 +49,5 @@ println("gradient: ", gradient(x -> build_tree(x, hyperparameters(0.1, 3, 1)).da
 
 f = max_depth -> length(build_tree(data, hyperparameters(0.1, max_depth, 1.0)).data)
 
-# Compute the gradient with respect to bin_size
 grad = gradient(f, 1.0)
 println("gradient with respect to bin_size: ", grad[1])
