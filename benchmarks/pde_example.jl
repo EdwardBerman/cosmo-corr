a07f95a647e61ae3aa2018212d5a60a2c9e25aec
EdwardBerman <10emb29@gmail.com>
2024-09-11 15:24:35 -0400

    gg

--- benchmarks/pde_example.jl
+++ benchmarks/pde_example.jl
@@ -1,6 +1,6 @@
 using SciMLSensitivity
 using DelimitedFiles, Plots
-using OrdinaryDiffEq, Optimization, OptimizationPolyalgorithms, Zygote
+using OrdinaryDiffEq, Zygote
 
 # Problem setup parameters:
 Lx = 10.0
@@ -8,7 +8,7 @@ x = 0.0:0.01:Lx
 dx = x[2] - x[1]
 Nx = size(x)
 
-u0 = exp.(-(x .- 3.0) .^ 2) # I.C
+u0 = exp.(-(x .- 3.0) .^ 2) # Initial condition
 
 ## Problem Parameters
 p = [1.0, 1.0]    # True solution parameters
@@ -40,54 +40,23 @@ end
 function heat(u, p, t, xtrs)
     # Model parameters
     a0, a1 = p
-    dx, Nx = xtrs #[1.0,3.0,0.125,100]
+    dx, Nx = xtrs 
     return 2.0 * a0 .* u + a1 .* d2dx(u, dx)
 end
 heat_closure(u, p, t) = heat(u, p, t, xtrs)
 
-# Testing Solver on linear PDE
+# Solver for the heat equation
 prob = ODEProblem(heat_closure, u0, tspan, p)
 sol = solve(prob, Tsit5(), dt = dt, saveat = t);
 arr_sol = Array(sol)
 
-plot(x, sol.u[1], lw = 3, label = "t0", size = (800, 500))
-plot!(x, sol.u[end], lw = 3, ls = :dash, label = "tMax")
-
-ps = [0.1, 0.2];   # Initial guess for model parameters
-function predict(θ)
-    Array(solve(prob, Tsit5(), p = θ, dt = dt, saveat = t))
-end
-
-## Defining Loss function
+## Loss function definition
 function loss(θ)
-    pred = predict(θ)
-    return sum(abs2.(predict(θ) .- arr_sol)), pred # Mean squared error
+    pred = Array(solve(prob, Tsit5(), p = θ, dt = dt, saveat = t))
+    return sum(abs2.(pred .- arr_sol)) # Mean squared error
 end
 
-l, pred = loss(ps)
-size(pred), size(sol), size(t) # Checking sizes
+# Compute the gradient of the loss function with respect to parameters p
+loss_gradient = gradient(θ -> loss(θ), p)
+println("Gradient of the loss with respect to parameters: ", loss_gradient)
 
-LOSS = []                              # Loss accumulator
-PRED = []                              # prediction accumulator
-PARS = []                              # parameters accumulator
-
-cb = function (θ, l, pred) #callback function to observe training
-    display(l)
-    append!(PRED, [pred])
-    append!(LOSS, l)
-    append!(PARS, [θ])
-    false
-end
-
-cb(ps, loss(ps)...) # Testing callback function
-
-# Let see prediction vs. Truth
-scatter(sol[:, end], label = "Truth", size = (800, 500))
-plot!(PRED[end][:, end], lw = 2, label = "Prediction")
-
-adtype = Optimization.AutoZygote()
-optf = Optimization.OptimizationFunction((x, p) -> loss(x), adtype)
-
-optprob = Optimization.OptimizationProblem(optf, ps)
-res = Optimization.solve(optprob, PolyOpt(), callback = cb)
-@show res.u # returns [0.999999999613485, 0.9999999991343996]
