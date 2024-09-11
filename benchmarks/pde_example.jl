d2a37c210a2732e14fb8f4ef87365e3992b373b1
EdwardBerman <10emb29@gmail.com>
2024-09-11 15:48:44 -0400

    making example more applicable

--- benchmarks/pde_example.jl
+++ benchmarks/pde_example.jl
@@ -8,55 +8,50 @@ x = 0.0:0.01:Lx
 dx = x[2] - x[1]
 Nx = size(x)
 
-u0 = exp.(-(x .- 3.0) .^ 2) # Initial condition
+# Initial condition for 500 particles, each starting at random positions
+num_particles = 500
+particle_positions = rand(num_particles) .* Lx
+u0 = zeros(length(x))
+for pos in particle_positions
+    idx = findfirst(x -> x > pos, x)
+    if !isnothing(idx)
+        u0[idx] += 1.0 # Adding particle at the nearest grid point
+    end
+end
 
 ## Problem Parameters
-p = [1.0, 1.0]    # True solution parameters
+p = [1.0]  # Only diffusion coefficient is left as parameter
 const xtrs = [dx, Nx]      # Extra parameters
 dt = 0.40 * dx^2    # CFL condition
 t0, tMax = 0.0, 1000 * dt
 tspan = (t0, tMax)
 t = t0:dt:tMax;
 
-## Definition of Auxiliary functions
-function ddx(u, dx)
-    """
-    2nd order Central difference for 1st degree derivative
-    """
-    return [[zero(eltype(u))]; (u[3:end] - u[1:(end - 2)]) ./ (2.0 * dx); [zero(eltype(u))]]
-end
-
 function d2dx(u, dx)
-    """
-    2nd order Central difference for 2nd degree derivative
-    """
     return [zero(eltype(u));
             (@view(u[3:end]) .- 2.0 .* @view(u[2:(end - 1)]) .+ @view(u[1:(end - 2)])) ./
             (dx^2)
             zero(eltype(u))]
 end
 
-## ODE description of the Physics:
 function heat(u, p, t, xtrs)
-    # Model parameters
-    a0, a1 = p
-    dx, Nx = xtrs 
-    return 2.0 * a0 .* u + a1 .* d2dx(u, dx)
+    a1 = p[1]  
+    dx, Nx = xtrs
+    return a1 .* d2dx(u, dx) 
 end
 heat_closure(u, p, t) = heat(u, p, t, xtrs)
 
-# Solver for the heat equation
 prob = ODEProblem(heat_closure, u0, tspan, p)
 sol = solve(prob, Tsit5(), dt = dt, saveat = t);
 arr_sol = Array(sol)
 
-## Loss function definition
-function loss(θ)
-    pred = Array(solve(prob, Tsit5(), p = θ, dt = dt, saveat = t))
-    return sum(abs2.(pred .- arr_sol)) # Mean squared error
+function final_particle_distribution(θ)
+    sol = solve(prob, Tsit5(), p = θ, dt = dt, saveat = t)
+    return sol.u[end]  
 end
 
-# Compute the gradient of the loss function with respect to parameters p
-loss_gradient = gradient(θ -> loss(θ), p)
-println("Gradient of the loss with respect to parameters: ", loss_gradient)
+final_state_gradient = gradient(θ -> sum(final_particle_distribution(θ)), p)
+println("Gradient of the final state with respect to diffusion constant: ", final_state_gradient)
 
+plot(x, sol.u[1], lw = 3, label = "Initial", size = (800, 500))
+plot!(x, sol.u[end], lw = 3, ls = :dash, label = "Final")
