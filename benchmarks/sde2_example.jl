e6bfab9567d27a71f522f40b49c222f79c037d69
EdwardBerman <10emb29@gmail.com>
2024-09-12 11:48:30 -0400

    .

--- benchmarks/sde2_example.jl
+++ benchmarks/sde2_example.jl
@@ -26,17 +26,19 @@ function sde_solve(p)
 end
 
 # p = [μ_ra, μ_dec, μ_g1, μ_g2, σ_ra, σ_dec, σ_g1, σ_g2]
-p = [0.05, 0.05, 0.05, 0.05, 0.1, 0.1, 0.1, 0.1]  
+p = [0.0, 0.0, 0.0, 0.0, 0.1, 0.1, 0.1, 0.1]  
 
-#sol = sde_solve(p)
+sol = sde_solve(p)
 
-#sol_ra = [sol.u[i][1] for i in 1:length(sol.u)]
-#sol_dec = [sol.u[i][2] for i in 1:length(sol.u)]
-#sol_g1 = [sol.u[i][3] for i in 1:length(sol.u)]
-#sol_g2 = [sol.u[i][4] for i in 1:length(sol.u)]
+sol_ra = [sol.u[i][1] for i in 1:length(sol.u)]
+sol_dec = [sol.u[i][2] for i in 1:length(sol.u)]
+sol_g1 = [sol.u[i][3] for i in 1:length(sol.u)]
+sol_g2 = [sol.u[i][4] for i in 1:length(sol.u)]
 
-@time begin
-    sample(p) = [sde_solve(p) for _ in 1:500]  # Sample the solution 500 times
-    grads = jacobian(p -> mean([mean(sol.u[:][1] + sol.u[:][2] + sol.u[:][3] + sol.u[:][4]) for sol in sample(p)]), p)
-end
-println("Gradient with respect to μ and σ: ", grads)
+println(scatterplot(sol.t, sol_ra))
+
+#@time begin
+#    sample(p) = [sde_solve(p) for _ in 1:500]  # Sample the solution 500 times
+#    grads = jacobian(p -> mean([mean(sol.u[:][1] + sol.u[:][2] + sol.u[:][3] + sol.u[:][4]) for sol in sample(p)]), p)
+#end
+#println("Gradient with respect to μ and σ: ", grads)
