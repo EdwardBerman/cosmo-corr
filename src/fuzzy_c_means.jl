b8592b8dff928eb996fffcd1183d6b2a045c361c
EdwardBerman <10emb29@gmail.com>
2024-09-17 19:43:57 -0400

    changes for zygote

--- src/fuzzy_c_means.jl
+++ src/fuzzy_c_means.jl
@@ -91,21 +91,9 @@ initial_weights = rand(ncols, n_clusters)
 centers, weights, iterations = fuzzy_c_means(data, n_clusters, initial_centers, initial_weights, 2.0)
 @info "Converged in $iterations iterations"
 
-weighted_shear_one = weighted_average(shear_one, weights)
-weighted_shear_two = weighted_average(shear_two, weights)
-
-fuzzy_galaxies = [[centers[1,i], centers[2,i], weighted_shear_one[i], weighted_shear_two[i]] for i in 1:n_clusters]
-fuzzy_distances = [(fuzzy_galaxies[i], 
-                    fuzzy_galaxies[j], 
-                    Vincenty_Formula(fuzzy_galaxies[i][1:2], fuzzy_galaxies[j][1:2])) for i in 1:n_clusters, j in 1:n_clusters if i < j] 
-
 function combine_vectors_to_matrix(vec1, datapoint, vec2)
     result = vcat(vec1, datapoint, vec2)
     return result
-end
-
-function filter_in_range(matrix::AbstractMatrix{T}, min_val::T, max_val::T) where T
-    return map(x -> (x > min_val && x <= max_val) ? x : zero(T), matrix)
 end
 
 function calculate_direction(x_1, x_2, y_1, y_2, z_1, z_2)
@@ -118,8 +106,8 @@ end
     
 function fuzzy_shear_estimator(fuzzy_distance)
     ra1, dec1, ra2, dec2 = fuzzy_distance[1][1], fuzzy_distance[1][2], fuzzy_distance[2][1], fuzzy_distance[2][2]
-    x1, y1, z1 = cos(ra1) * cos(dec1), sin(ra1) * cos(dec1), sin(dec1)
-    x2, y2, z2 = cos(ra2) * cos(dec2), sin(ra2) * cos(dec2), sin(dec2)
+    x1, y1, z1 = cos(ra1 * π / 180) * cos(dec1 * π / 180), sin(ra1 * π / 180) * cos(dec1 * π / 180), sin(dec1 * π / 180)
+    x2, y2, z2 = cos(ra2 * π / 180) * cos(dec2 * π / 180), sin(ra2 * π / 180) * cos(dec2 * π / 180), sin(dec2 * π / 180)
 
     r21 = calculate_direction(x2, x1, y2, y1, z2, z1)
     ϕ21 = real(conj(r2) * r2 / norm(r2)^2) # rotating 2 in the direction of 1
@@ -147,11 +135,10 @@ function fuzzy_shear_estimator(fuzzy_distance)
     return dot(object_one_shear_one_rotated, object_two_shear_two_rotated) + dot(object_one_shear_two_rotated, object_two_shear_one_rotated)
 end
 
-function fuzzy_correlator(ra, 
-        dec, 
-        quantity_one, 
-        quantity_two, 
-        nclusters, 
+function fuzzy_correlator(ra::Vector{Float64}, 
+        dec::Vector{Float64},
+        quantity_one::Vector{fuzzy_shear},
+        quantity_two::Vector{fuzzy_shear},
         initial_centers, 
         initial_weights,
         θ_min,
@@ -184,7 +171,7 @@ function fuzzy_correlator(ra,
                         Vincenty_Formula(fuzzy_galaxies[i][1:2], fuzzy_galaxies[j][1:2])) for i in 1:nclusters, j in 1:nclusters if i < j]
     bins = 10 .^ range(log10(θ_min), log10(θ_max), length=number_bins)
     binned_fuzzy_distances = [[fuzzy_distance for fuzzy_distance in fuzzy_distances if bins[i] < fuzzy_distance[3] <= bins[i+1]] for i in 1:length(bins)-1]
-    fuzzy_estimates = [[fuzzy_shear_estimator(fuzzy_distance) for fuzzy_distance in fuzzy_distance_bin] for fuzzy_distance_bin in binned_fuzzy_distances]
+    fuzzy_estimates = [fuzzy_shear_estimator(fuzzy_distance) for fuzzy_distance_bin in binned_fuzzy_distances for fuzzy_distance in fuzzy_distance_bin]
     fuzzy_correlations = [sum(fuzzy_estimate)/(2*length(fuzzy_estimate)) for fuzzy_estimate in fuzzy_estimates]
     return fuzzy_correlations
 end
