6af9430b7670b96abf6504d990775caf6c953ebf
EdwardBerman <10emb29@gmail.com>
2024-09-19 08:59:31 -0400

    refactor

--- benchmarks/naive_rho_stats.jl
+++ benchmarks/naive_rho_stats.jl
@@ -9,22 +9,24 @@ using Statistics
 using Distributions
 
 println(nthreads())
+struct fuzzy_shear
+    shear::Vector{Float64}
+end
 
-file_name = "psf_fits_f277w.fits"
+
+file_name = "revised_apr_f115w_shopt_xy_info.fits"
 
 fits = pyimport("astropy.io.fits")
 f = fits.open(file_name)
-print(f[2].data)
 
-ra = f[2].data["ra"]
-dec = f[2].data["dec"]
-σ_D = f[2].data["sig_vignet"]
-g1_D = f[2].data["g1_vignet"]
-g2_D = f[2].data["g2_vignet"]
-σ_psf = f[2].data["sig_psfex"]
-g1_psf = f[2].data["g1_psfex"]
-g2_psf = f[2].data["g2_psfex"]
-χ2 = f[2].data["chi2"]
+ra = f[1].data["ra"]
+dec = f[1].data["dec"]
+σ_D = f[1].data["sig_vignet"]
+g1_D = f[1].data["g1_vignet"]
+g2_D = f[1].data["g2_vignet"]
+σ_psf = f[1].data["sig_psfex"]
+g1_psf = f[1].data["g1_psfex"]
+g2_psf = f[1].data["g2_psfex"]
 
 ra = convert(Vector{Float64}, ra)
 dec = convert(Vector{Float64}, dec)
@@ -34,39 +36,48 @@ g2_D = convert(Vector{Float64}, g2_D)
 σ_psf = convert(Vector{Float64}, σ_psf)
 g1_psf = convert(Vector{Float64}, g1_psf)
 g2_psf = convert(Vector{Float64}, g2_psf)
-χ2 = convert(Vector{Float64}, χ2)
 
 # NOTE: Shear is not a vector, but a complex number. However, we represent is as a vector of two components for quick computation and readibility.
 e_D = [[g1_D[i], g2_D[i]] for i in 1:length(g1_D)]
+
 e_psf = [[g1_psf[i], g2_psf[i]] for i in 1:length(g1_psf)]
+e_psf_conj = [[g1_psf[i], -g2_psf[i]] for i in 1:length(g1_psf)]
+
 δ_e = [[g1_D[i] - g1_psf[i], g2_D[i] - g2_psf[i]] for i in 1:length(g1_D)]
+δ_e_conj = [[g1_D[i] - g1_psf[i], -g2_D[i] + g2_psf[i]] for i in 1:length(g1_D)]
 
 T_D = 2.0 .* σ_D.^2
 T_psf = 2.0 .* σ_psf.^2
 δ_T = T_D .- T_psf
 δ_TT = δ_T ./ T_psf
 
-e_psf_χ2 = e_psf .* χ2
-
-# At some point i should go from g1 g2 to gtan gcross
+ra_dec = hcat(ra, dec)
+n_clusters = 50
+nrows, ncols = size(data)
+initial_centers = kmeans_plusplus_weighted_initialization_vincenty(ra_dec', n_clusters, rand(length(ra_dec)), 0.5)
+initial_weights = rand(ncols, n_clusters)
 
 println("Computing ρ1")
-ρ1 = corr(ra, dec, δ_e, δ_e, 200.0*60*0.03/3600, 10, 5000.0*60*0.03/3600; correlator=naivecorr, verbose=true)
+fuzzy_shear_one = [fuzzy_shear(δ_e_conj[i]) for i in 1:length(δ_e)]
+fuzzy_shear_two = [fuzzy_shear(δ_e[i]) for i in 1:length(δ_e)]
+ρ1 = fuzzy_correlator(ra, dec, fuzzy_shear_one, fuzzy_shear_two, initial_centers, initial_weights, n_clusters, 200.0*60*0.03/3600, 10, 5000.0*60*0.03/3600; spacing="log", verbose=true)
 
 println("Computing ρ2")
-ρ2 = corr(ra, dec, e_psf, δ_e, 200.0*60*0.03/3600, 10, 5000.0*60*0.03/3600; correlator=naivecorr, verbose=true)
+fuzzy_shear_one = [fuzzy_shear(e_psf_conj[i]) for i in 1:length(e_psf)]
+fuzzy_shear_two = [fuzzy_shear(δ_e[i]) for i in 1:length(δ_e)]
+ρ2 = fuzzy_correlator(ra, dec, fuzzy_shear_one, fuzzy_shear_two, initial_centers, initial_weights, n_clusters, 200.0*60*0.03/3600, 10, 5000.0*60*0.03/3600; spacing="log", verbose=true)
 
 println("Computing ρ3")
-ρ3 = corr(ra, dec, e_psf.*δ_TT, e_psf.*δ_TT, 200.0*60*0.03/3600, 10, 5000.0*60*0.03/3600; correlator=naivecorr, verbose=true)
+fuzzy_shear_one = [fuzzy_shear(e_psf_conj[i] * δ_TT[i]) for i in 1:length(e_psf)]
+fuzzy_shear_two = [fuzzy_shear(e_psf[i] * δ_TT[i]) for i in 1:length(e_psf)]
+ρ3 = fuzzy_correlator(ra, dec, fuzzy_shear_one, fuzzy_shear_two, initial_centers, initial_weights, n_clusters, 200.0*60*0.03/3600, 10, 5000.0*60*0.03/3600; spacing="log", verbose=true)
 
 println("Computing ρ4")
-ρ4 = corr(ra, dec, δ_e, e_psf.*δ_TT, 200.0*60*0.03/3600, 10, 5000.0*60*0.03/3600; correlator=naivecorr, verbose=true)
+fuzzy_shear_one = [fuzzy_shear(δ_e_conj[i]) for i in 1:length(δ_e)]
+fuzzy_shear_two = [fuzzy_shear(e_psf[i] * δ_TT[i]) for i in 1:length(e_psf)]
+ρ4 = fuzzy_correlator(ra, dec, fuzzy_shear_one, fuzzy_shear_two, initial_centers, initial_weights, n_clusters, 200.0*60*0.03/3600, 10, 5000.0*60*0.03/3600; spacing="log", verbose=true)
 
 println("Computing ρ5")
-ρ5 = corr(ra, dec, e_psf, e_psf.*δ_TT, 200.0*60*0.03/3600, 10, 5000.0*60*0.03/3600; correlator=naivecorr, verbose=true)
-
-println("Computing ρ6")
-ρ6 = corr(ra, dec, e_psf_χ2, e_psf_χ2, 200.0*60*0.03/3600, 10, 5000.0*60*0.03/3600; correlator=naivecorr, verbose=true)
-
-println("Computing ρ7")
-ρ7 = corr(ra, dec, δ_e, e_psf_χ2, 200.0*60*0.03/3600, 10, 5000.0*60*0.03/3600; correlator=naivecorr, verbose=true)
+fuzzy_shear_one = [fuzzy_shear(e_psf_conj[i]) for i in 1:length(e_psf)]
+fuzzy_shear_two = [fuzzy_shear(e_psf[i] * δ_TT[i]) for i in 1:length(e_psf)]
+ρ5 = fuzzy_correlator(ra, dec, fuzzy_shear_one, fuzzy_shear_two, initial_centers, initial_weights, n_clusters, 200.0*60*0.03/3600, 10, 5000.0*60*0.03/3600; spacing="log", verbose=true)
