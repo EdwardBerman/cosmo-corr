5be15b16219e816062a68ae58e22d2ebdf475087
EdwardBerman <10emb29@gmail.com>
2024-12-20 02:53:01 -0500

    updating toy plots

--- assets/toy.py
+++ assets/toy.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
 import numpy as np
 
 # Data for the histogram
@@ -10,7 +11,8 @@ red_data = np.array([2, 8, 3])
 x = np.arange(bar_count)
 
 # Use the xkcd plotting style
-plt.xkcd()
+#plt.xkcd()
+plt.rc('font', family='monospace')
 
 plt.figure(figsize=(10, 6))
 
@@ -19,9 +21,9 @@ plt.bar(x, blue_data, color='blue', label='True Distribution')
 plt.bar(x, red_data, bottom=blue_data, color='pink', label='Binned Approximate Distribution')
 
 # Adding labels and title
-plt.xlabel(r'$\theta$')
-plt.ylabel(r'$DD(\theta)$')
-plt.title(r'True vs Approximate $DD(\theta$)')
+plt.xlabel(r'$\theta$', fontsize=14)
+plt.ylabel(r'$DD(\theta)$', fontsize=14)
+plt.title(r'True vs Approximate $DD(\theta$)', fontsize=14)
 
 # Adding a legend
 plt.legend(loc='lower right')
@@ -31,7 +33,18 @@ plt.annotate('', xy=(0.5, 6), xytext=(0, 6),
              arrowprops=dict(facecolor='black', shrink=0.05))
 plt.annotate('', xy=(1.5, 6), xytext=(2, 6),
              arrowprops=dict(facecolor='black', shrink=0.05))
-plt.text(0.0, 6.5, "Binning Error", fontsize=12, verticalalignment='center')
-plt.text(1.45, 6.5, "Binning Error", fontsize=12, verticalalignment='center')
+
+def add_outlined_text(x, y, text, fontsize=14):
+    t = plt.text(x, y, text, fontsize=fontsize, fontweight='bold', color='black',
+                 verticalalignment='center', horizontalalignment='center')
+    t.set_path_effects([path_effects.Stroke(linewidth=3, foreground='white'),
+                        path_effects.Normal()])
+    return t
+
+add_outlined_text(0.25, 6.5, "Binning Error")
+add_outlined_text(1.75, 6.5, "Binning Error")
+
+#plt.text(0.0, 6.5, "Binning Error", fontsize=12, verticalalignment='center', fontweight='bold')
+#plt.text(1.45, 6.5, "Binning Error", fontsize=12, verticalalignment='center', fontweight='bold')
 # Show the plot
 plt.savefig('toy.png')
